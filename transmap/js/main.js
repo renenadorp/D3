b96a14d0736b5004932b5ce1dc4cbb8fc21a71e8
renenadorp <rene@rnadorp.nl>
2022-10-28 16:23:39 +0200

    Approved

--- transmap/js/main.js
+++ transmap/js/main.js
@@ -14,9 +14,14 @@ const svg     = d3.select("#viz-area").append("svg")
   					.attr("height", CANVAS.HEIGHT + MARGIN.TOP  + MARGIN.BOTTOM)
 const svgCanvas = svg.append("g")
   					.attr("transform", `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`)
+const gYearBox  = svgCanvas.append('g').attr('id', 'gYearBox')
+
+var gMap = svgCanvas.append("g").attr("class", "gMap")
+                      .attr("transform", `translate(${0},${YEARBOX.HEIGHT})`)
 
 //TIP
-var tip = d3.tip().attr('class', 'd3-tip').html(d=>{ 
+
+var tip = d3.tip().attr('class', 'd3-tip').html((EVENT,d)=> {
                 let text =`
                 
                 <div class="container">
@@ -26,9 +31,6 @@ var tip = d3.tip().attr('class', 'd3-tip').html(d=>{
                 </div>                        
                         
                         `
-                
-                
-                
                 
                 return text;      })
 svgCanvas.call(tip)
@@ -41,11 +43,13 @@ fetch(myRequest, {method: 'GET',
                   headers: {"Content-type": "application/json" } })
   .then((response) => response.json())
   .then((data) => {
-      updateTMap(data)
+    globalThis.updatedData = data;
+    updateTMap(updatedData);
+    updateProjects(updatedData);
+    updateProjectList(updatedData);
+
   })
-
 function updateTMap(data){
-
     const DIMCOUNT  = data.dims.names.length
     const YEARCOUNT = data.years.length
     const TLINES    = []
@@ -53,7 +57,6 @@ function updateTMap(data){
     const YEARWIDTH = (YEARBOX.WIDTH / YEARCOUNT)-YEARPAD 
 
     //YEARS
-    const gYearBox  = svgCanvas.append('g').attr('id', 'gYearBox')
     const gYear     = gYearBox.selectAll().data(data.years)
     const gYearEnter= gYear.enter().append('g').attr('transform', (d,i)=> {return `translate(${(i*(YEARWIDTH+YEARPAD))},${5   })`})
 
@@ -73,8 +76,6 @@ function updateTMap(data){
     const TMAP = {    HEIGHT: CANVAS.HEIGHT-YEARBOX.HEIGHT, 
                     WIDTH : CANVAS.WIDTH}
 
-    const gMap = svgCanvas.append("g").attr("id", "gMap")
-                .attr("transform", `translate(${0},${YEARBOX.HEIGHT})`)
 
     gMap.append("rect")
         .attr("width" , TMAP.WIDTH)
@@ -144,7 +145,6 @@ function updateTMap(data){
                 .attr('class', 'dimension')
                 .attr('id', `TLine-${i}`)
         }
-
 
     //TRANSFORMATION CURVES      
     function TCurve(d,i,c) {
@@ -232,7 +232,6 @@ function updateTMap(data){
         .attr('class', 'State')
         .attr('dy', 5)
 
-
   
     //console.log(data.dims.details)
 
@@ -242,21 +241,8 @@ function updateTMap(data){
         .attr('href', 'assets/hso.png')
         .attr('width', 145)
 
-    const gProjects = gMap.append('g').attr('class', 'gProjects')
-    const sProjects = gProjects.selectAll('.gProject').data(data.projects)
-    sProjects.exit().remove()
+    globalThis.gProjects = gMap.append('g').attr('id', 'gProjects');
 
-    gProject = sProjects.enter().append('g').attr('class', 'gProject').attr( 'transform', d=>`translate(${d.cx},${d.cy})`)
-    gProject.append('circle')
-        .attr('class', 'Project')
-        .attr( 'r', 20)
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide)
-        
-    gProject.append('text').text(d=>d.number).attr('fill', 'white').attr('dx', -5).attr('dy', 5)
-        
-
-    
     //******************************************************************************************************************* */
     // INTERSECTIONS: LINE-CURVE
     // Credits: https://bl.ocks.org/bricof
@@ -275,7 +261,7 @@ function updateTMap(data){
     // console.log(TLINES)
 
     pts_i = path_line_intersections(pathEl,line)
-    console.log(pts_i)
+    //console.log(pts_i)
     for (i=0; i< pts_i.length; i++){
         if (pts_i[i].x!==1000){
             gMap.append('circle')
@@ -331,7 +317,334 @@ function updateTMap(data){
       
       }
 
+    return;
+    }
+
+function updateProjects(data){
+    //console.log('data:', data);
+
+    const t = d3.transition()
+    .duration(4000)
+    .ease(d3.easeLinear);
+
+    var Projects = gProjects.selectAll('.gProject').data(data.projects)
+    Projects
+        .join(
+            function(enter) {
+                gProjectEnter = enter.append('g').attr('class', 'gProject').call(drag());
+
+                gProjectEnter
+                    .attr('id', d => {return 'Project-' + String(d.number).padStart(2, '0')})
+                    .attr( 'transform', d=>{ //console.log('here'); 
+                        return `translate(${d.cx },${d.cy})`})//.transition(t)
+                gProjectEnterRect = 
+                    gProjectEnter.append('rect')
+                    .attr('class', d => `Project ${d.status}`)
+                    .attr('id', d => `Project-${String(d.number).padStart(2, '0')}`)
+                    //.attr('width', d=>{console.log(d.name.length); return d.name.length *7 + 40})
+                    .attr('height', 30)
+                    .attr('x', d => -20)
+                    .attr('y', d => - 15)
+                    ;
+                gProjectEnterText = 
+                gProjectEnter.append('text')
+                    .attr('class', 'Project')
+                    .text(d=>{return `${d.number}.${d.name}`})
+                    .attr('fill', 'white').attr('dx', -5).attr('dy', 5)
+                    .attr('text-anchor', 'start');
+
+                gProjectEnterRect.attr('width',  d=>BrowserText.getWidth(`${d.number}.${d.name}`, '14', 'Arial')+25)
+                //console.log('textlength:', gProjectEnterText.node().getComputedTextLength());
+
+/*
+                gProjectEnterCircle = 
+                gProjectEnter.append('circle')
+                .attr('cx', -10)
+                .attr('cy', -15)
+                .attr('r', 3)
+                .attr('class', 'project-hinge')
+                
+              gProjectEnterPushPin = 
+                gProjectEnter.append('image')
+                .attr('class', 'project-hinge')
+                .attr( 'x', -200)
+                .attr('y', -200)
+                .attr('href', "assets/pushpin.svg" )
+                .attr("transform",   'scale(.15)');
+*/
+       
+
+                Projects = gProjectEnter.merge(Projects)
 
 
+            },
+            function(update){
+                gProjectUpdate = update
+                 .attr( 'transform', d=>{ console.log('update'); 
+                    return `translate(${d.cx },${d.cy})`}).transition(t)
+                update.select('text.Project').text(d=>{return `${d.number}.${d.name}`})
+                .attr('fill', 'white').attr('dx', -5).attr('dy', 5)
+                .attr('text-anchor', 'start')
+            },
+
+            function(exit) {
+                return exit.remove();
+              }
+        )
+}
+
+function moveProject(id, x, y){
+    
+    const projectArrayIndex = +id.substring(8,10)-1
+    updatedData.projects[projectArrayIndex].cx = x;
+    updatedData.projects[projectArrayIndex].cy = y;
+    
+}
+
+function changeProject(what, e){
+    projectSelector = `rect#${e.id.substring(0, 10)}`; //
+    projectArrayIndex = +e.id.substring(9,10)-1 //    Project-01-Name
+    //console.log(what, ' ' , e, updatedData, 'id:', e.id, 'number:', projectArrayIndex )
+
+    if (what=="Name") {
+        updatedData.projects[projectArrayIndex].name = e.value;
+        updateProjects(updatedData)
+
+    }
+    if (what=="Status") {
+        updatedData.projects[projectArrayIndex].status = e.value;
+        //console.log( projectSelector)
+        var SelectedStatus = document.getElementById( e.id );
+        var SelectedStatusValue = SelectedStatus.options[ SelectedStatus.selectedIndex ].value
+        var SelectedProject =document.querySelector(projectSelector)
+        SelectedProject.className.baseVal = 'Project ' + SelectedStatusValue;
+        console.log('Status:' ,SelectedStatus, SelectedStatusValue, SelectedProject)
+
+    }
+
+    return;
+}
+function updateProjectList(data)  {
+    const tableStringStart = 
+    `
+    <table class="table table-sm table-light" id="project-list">
+                    <thead>
+                        <tr>
+                        <th scope="col" class="text-right">Nbr</th>
+                        <th scope="col" class="text-left">Name</th>
+                        <th scope="col" class="text-left">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+    `;
+
+    var tableStringRows = '';
+    data.projects.map(row=> {
+    tableStringRows += `
+    <tr>
+    <th scope="row" class="text-right">${row.number}</th>
+    <td><input type="text" id="Project-${String(row.number).padStart(2, '0')}-Name" class="form-control project-name" value="${row.name}"></input></td>    
+    <td><select name="status" id="Project-${String(row.number).padStart(2, '0')}-Status" class="form-control form-select-lg project-status" aria-label=".form-select-sm form-control project-status" value="${row.status}">
+            <option name="completed" value="completed" ${row.status=='completed' ? "selected" : null }>Completed</option>
+            <option name="inprogress" value="inprogress" ${row.status=='inprogress' ? "selected" : null }>In Progress</option>
+            <option name="future" value="future" ${row.status=='future' ? "selected" : null }>Future</option>
+        
+        </select></td>    
+    
+
+    </tr>
+    `});
+    
+    const tableStringEnd =
+        `
+        <tr><td></td><td><a href="" id="a"></a>
+        <button type="button" class="btn btn-primary" onclick="saveMap()" id="saveMap" >Save Map</button>
+       </td></tr>
+        </tbody>
+        </table>
+        `;
+
+    const html = tableStringStart + tableStringRows + tableStringEnd;
+    d3.select("#project-list").html(html);
+    //console.log( d3.select("#project-list"))
+       
+    d3.selectAll(".move-project").on("click", function() {
+        moveProject(this.id);
+    });
+
+    d3.selectAll(".project-name").on("change", function() {
+        changeProject("Name", this);
+    });
+    d3.selectAll(".project-status").on("change", function() {
+        changeProject("Status", this);
+    });
+    d3.selectAll("button#saveMap").on("click", function(){
+        saveMap(data)
+    });
+
+    return ;
 
 }
+
+function saveMap (data) {
+    var json = JSON.stringify(data)
+    //console.log('saveMap:', json)
+    //alert('savemap ')
+// Function to download data to a file
+    var type ="application/json";
+    var filename = "data.json";
+    var jsonBlob = new Blob([json], {type: type});
+
+    //var jsonBlob = new Blob([JSON.stringify("kiki")], { type: 'application/javascript;charset=utf-8' });
+    var link=window.URL.createObjectURL(jsonBlob);
+    //window.location=link;
+    saveAs(jsonBlob, filename);
+
+    
+
+
+    
+   return; 
+}
+
+function drag () {
+    
+    function dragstarted(event, d) {
+      d3.select(this).raise().attr("stroke", "black");
+    }
+  
+    function dragged(event, d) {
+        //d3.select(this).attr("x", d.x = event.x).attr("y", d.y = event.y);
+      d3.select(this).attr("transform", `translate(${event.x},${event.y})`);
+
+    }
+  
+    function dragended(event, d) {
+      d3.select(this).attr("stroke", null);
+      moveProject(this.id, event.x, event.y);
+
+    }
+  
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+  }
+
+  var BrowserText = (function () {
+    var canvas = document.createElement('canvas'),
+        context = canvas.getContext('2d');
+    /**
+     * Measures the rendered width of arbitrary text given the font size and font face
+     * @param {string} text The text to measure
+     * @param {number} fontSize The font size in pixels
+     * @param {string} fontFace The font face ("Arial", "Helvetica", etc.)
+     * @returns {number} The width of the text
+     **/
+    function getWidth(text, fontSize, fontFace) {
+        context.font = fontSize + 'px ' + fontFace;
+        console.log('width:', context.measureText(text).width)
+
+        return context.measureText(text).width;
+    }
+
+    return {
+        getWidth: getWidth
+    };
+})();
+
+
+
+/*
+  function updateProjectListOLD(data)  {
+    const tableStringStart = 
+    `
+    <table class="table table-sm table-light" id="project-list">
+                    <thead>
+                        <tr>
+                        <th scope="col" class="text-right">Nbr</th>
+                        <th scope="col" class="text-left">Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+    `;
+
+    var tableStringRows = '';
+    data.projects.map(row=> {
+
+    tableStringRows += `
+    <tr>
+    <th scope="row" class="text-right">${row.number}</th>
+    <td><input type="text" id="Name-${row.number}"class="form-control project-name" value="${row.name}"></input>
+    </td>    
+    <td><button type="button" id ="Project-${String(row.number).padStart(2, '0')}-Up" class="btn btn-light move-project">
+        <span class="bi bi-arrow-up-circle">
+        <img src = "assets/arrow-up-blue.png" width="20px" height="20px"/>
+
+        </span>
+        </button></td>
+        <td>
+            <button type="button" id ="Project-${String(row.number).padStart(2, '0')}-Down" class="btn btn-light move-project">
+            <span class="bi bi-arrow-down-circle">
+            <img src = "assets/arrow-up-blue.png" width="20px" height="20px"/>
+        </span>
+            </button>
+        </td>
+        <td>
+            <button type="button" id ="Project-${String(row.number).padStart(2, '0')}-Left" class="btn btn-light move-project">
+            <span class="bi bi-arrow-left-circle">
+            <img src = "assets/arrow-up-blue.png" width="20px" height="20px"/>
+        </span>
+            </button>
+        </td>
+        <td>
+            <button type="button" id ="Project-${String(row.number).padStart(2, '0')}-Right" class="btn btn-light move-project">
+            <span class="bi bi-arrow-right-circle">
+            <img src = "assets/arrow-up-blue.png" width="20px" height="20px"/>
+        </span>
+            </button>
+        </td>
+    </tr>
+    `});
+    
+    const tableStringEnd =
+        `
+        </tbody>
+        </table>
+        `;
+
+    const saveButton = `
+    <button type="button" class="btn btn-primary" id="saveMap" >Save Map</button>
+    `        
+    const html = tableStringStart + tableStringRows + tableStringEnd + saveButton;
+    d3.select("#project-list").html(html);
+    //console.log( d3.select("#project-list"))
+       
+    d3.selectAll(".move-project").on("click", function() {
+        moveProject(this.id);
+    });
+
+    d3.selectAll(".project-name").on("change", function() {
+        changeProject("Name", this);
+    });
+    d3.select('#saveMap').on("click", function() { saveMap(data);})
+
+    return ;
+
+}  
+
+function moveProjectOLD(id, c){
+    //console.log('updatedData (before):', updatedData)
+    
+    const projectArrayIndex = +id.substring(8,10)-1
+    const moveDirection     = id.substring(11,12)
+    //console.log('project:',updatedData.projects[projectArrayIndex], ' Axis:', projectAxis)
+    if (moveDirection=='U') updatedData.projects[projectArrayIndex].cy-=10;
+    if (moveDirection=='D') updatedData.projects[projectArrayIndex].cy+=10;
+    if (moveDirection=='L') updatedData.projects[projectArrayIndex].cx-=10;
+    if (moveDirection=='R') updatedData.projects[projectArrayIndex].cx+=10;
+
+    updateProjects(updatedData)
+    
+}
+*/
