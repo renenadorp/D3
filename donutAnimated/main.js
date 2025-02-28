bf94e8daa484c0b0f288f85034dd6912a3ef84c9
Rene Nadorp <rene@rnadorp.nl>
2025-02-28 07:49:45 +0100

    Animation works, including labels (almost)

--- donutAnimated/main.js
+++ donutAnimated/main.js
@@ -1,34 +1,34 @@
-import {DonutChart} from './donut.js';
+import { DonutChart } from './donut.js';
 
 const INPUT_FILE = 'data/dama.xlsx'
 const margin = { top: 0, right: 120, bottom: 10, left: 100 };
-const width  = 1000;
+const width = 1000;
 const height = 1000;
-const colorScaleFill = d3.scaleLinear().domain([0,9]).range([   "white", "#E8003D"])
+const colorScaleFill = d3.scaleLinear().domain([0, 9]).range(["white", "#E8003D"])
 let chartData;
 let valueColumn = 'valueDama';
 let focusColumn = 'focusDama';
 
 const svg = d3.select('#viz')
-.append('svg')
-  .attr('width'  , width  + margin.left + margin.right)
-  .attr('height' , height + margin.top  + margin.bottom)
-.append('g')
-  .attr('transform', `translate(${width/2}, ${height/2}) scale(0.8)`);
-
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', `translate(${width / 2}, ${height / 2}) scale(0.8)`);
+let labelGroup;
 const settings = [
   {
     sheet: "Dama",
-    colorScaleFont : d3.scaleOrdinal().domain([0,9]).range([  "#E8003D", "white",       ]),
-    colorScaleFill : d3.scaleLinear().domain([0,9]).range([   "white", "#E8003D",       ]), 
-    label:  { text: "Data\nGovernance", FontColor: "#E8003D"},
-    pie:    { stroke:  "black", strokeWidth: 1}
+    colorScaleFont: d3.scaleOrdinal().domain([0, 9]).range(["#E8003D", "white",]),
+    colorScaleFill: d3.scaleLinear().domain([0, 9]).range(["white", "#E8003D",]),
+    label: { text: "Data\nGovernance", FontColor: "#E8003D" },
+    pie: { stroke: "black", strokeWidth: 1 }
 
   }];
 
 const SCHEMAS =
   [
-        {
+    {
       'name': {
         prop: 'name',
         type: String
@@ -55,80 +55,101 @@ const SCHEMAS =
       }
 
 
-    }    
+    }
 
   ]
 
-let F;
-
-export const chart = () => {
+export const chart = (init) => {
   // const height = Math.min(500, width / 2);
+  console.log('chart function called!!!', init)
 
-  
   const outerRadius = 500;
   const innerRadius = outerRadius * 0.3;
-  const labelRadius = (outerRadius+innerRadius) / 2
+  const labelRadius = (outerRadius + innerRadius) / 2
   const arc = d3.arc()
-        .innerRadius(innerRadius)
-        .outerRadius(outerRadius);
-  const arcLabel  = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius);
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius);
+  const arcLabel = d3.arc().innerRadius(labelRadius).outerRadius(labelRadius);
 
 
   const pie = d3.pie().sort(null).value((d) => d[valueColumn]);
 
 
   const path = svg.datum(chartData).selectAll("path")
-      .data(pie)
-      .join("path")
-        .attr("fill", (d, i) => {
-          // console.log(d.data[focusColumn]); 
-          return colorScaleFill(d.data[focusColumn])})
-        .attr("d", arc)
-        .attr("stroke", "black")
-      .each(function(d) { this._current = d; }) // store the initial angles
-        ; 
-  svg.append("g")
+    .data(pie)
+    .join("path")
+    .attr("fill", (d, i) => {
+      // console.log(d.data[focusColumn]); 
+      return colorScaleFill(d.data[focusColumn])
+    })
+    .attr("d", arc)
+    .attr("stroke", "black")
+    .each(function (d) { this._current = d; }) // store the initial angles
+    ;
+  if (init) {
+    labelGroup =
+      svg.append("g")
         .attr("font-family", "sans-serif")
-        .attr("font-size",28)
+        .attr("font-size", 28)
         .attr("text-anchor", "middle")
-      .selectAll("text")
-      .data(pie)
-      .join("text")
-        .attr("transform", d => {
-          const rotation = d.endAngle < Math.PI ? 
-            (d.startAngle/2 + d.endAngle/2) * 180/Math.PI : 
-            (d.startAngle/2  + d.endAngle/2 + Math.PI) * 180/Math.PI ;
-          return `translate(${arcLabel.centroid(d)}), rotate(${rotation-90})`
-        }).attr("fill", d=> { 
-          return "black"
-        })
-      .selectAll("tspan")
-      .data(d => {
-        const lines = d.data[valueColumn]>0 ?  `${d.data.name}`.split(/\n/) : "";
-        // console.log(lines, title);
-        return (d.endAngle - d.startAngle) > 0.25 ? lines : lines.slice(0, 1);
-      })
-      .join("tspan")
-        .attr("x", 0)
-        .attr("y", (_, i) => `${i * 1.1}em`)
-        .attr("font-weight", (_, i) => i ? null : "regular")
-        .text(d => {
-          // console.log( d);
-          return d});
+      ;
+  }
+  labelGroup.selectAll("text")
 
- function change(value) {
+    .data(pie)
+    .join("text")
+    .attr('class', 'pieLabel')
+    .attr("transform", d => {
+      const rotation = d.endAngle < Math.PI ?
+        (d.startAngle / 2 + d.endAngle / 2) * 180 / Math.PI :
+        (d.startAngle / 2 + d.endAngle / 2 + Math.PI) * 180 / Math.PI;
+      return `translate(${arcLabel.centroid(d)}), rotate(${rotation - 90})`
+    })
+    .attr("fill", d => "black")
+    .selectAll("tspan")
+    .data(d => {
+      const lines = d.data[valueColumn] > 0 ? `${d.data.name}`.split(/\n/) : "";
+      // console.log(lines, title);
+      return (d.endAngle - d.startAngle) > 0.25 ? lines : lines.slice(0, 1);
+    })
+    .join("tspan")
+    .attr("x", 0)
+    .attr("y", (_, i) => `${i * 1.1}em`)
+    .attr("font-weight", (_, i) => i ? null : "regular")
+    .text(d => {
+      // console.log( d);
+      return d
+    });
 
-   valueColumn = 'value' + value;
-   focusColumn = 'focus' + value;
-  //  console.log('value:', value, valueColumn, focusColumn)
-    
-   
+  function change(value) {
+
+    valueColumn = 'value' + value;
+    focusColumn = 'focus' + value;
+    //  console.log('value:', value, valueColumn, focusColumn)
+
+
     pie.value((d) => d[valueColumn]); // change the value function
     path.data(pie); // compute the new angles
     path.transition().duration(1750)
       .attrTween("d", arcTween) // redraw the arcs
       .attr("fill", d => colorScaleFill(d.data[focusColumn]))
-    return 
+
+
+
+    labelGroup
+      .selectAll("text")
+      .data(pie)
+      .transition().duration(1750)
+
+      .attr("transform", d => {
+        const rotation = d.endAngle < Math.PI ?
+          (d.startAngle / 2 + d.endAngle / 2) * 180 / Math.PI :
+          (d.startAngle / 2 + d.endAngle / 2 + Math.PI) * 180 / Math.PI;
+        return `translate(${arcLabel.centroid(d)}), rotate(${rotation - 90})`
+      })
+
+
+    return
   }
 
   // Store the displayed angles in _current.
@@ -141,31 +162,34 @@ export const chart = () => {
   }
 
   // Return the svg node to be displayed.
-  return Object.assign(svg.node(), {change});
-  
+  return Object.assign(svg.node(), { change });
+
 }
 
 
-export function main() {
-  // svg.select('#chart').remove();
-  let dataUnfiltered = {};
-  let promiseList = [];
+export function main(init) {
+  if (init) {
+    // svg.select('#chart').remove();
+    let dataUnfiltered = {};
+    let promiseList = [];
 
-  promiseList.push(fetch(INPUT_FILE).then(response => response.blob()).then(blob => readXlsxFile(blob, { sheet: 1, schema: SCHEMAS[0] }).then((resolve, reject) => { dataUnfiltered = resolve.rows; 
-    // console.log('reject',reject, resolve)
-  })))        
-  Promise.all(promiseList)
-    .then((resolve, reject) => {
-      // Data Filtering based on UI      
-      // dataFiltered = datasUnfiltered.filter(e => (....))
+    promiseList.push(fetch(INPUT_FILE).then(response => response.blob()).then(blob => readXlsxFile(blob, { sheet: 1, schema: SCHEMAS[0] }).then((resolve, reject) => {
+      dataUnfiltered = resolve.rows;
+      // console.log('reject',reject, resolve)
+    })))
+    Promise.all(promiseList)
+      .then((resolve, reject) => {
+        // Data Filtering based on UI      
+        // dataFiltered = datasUnfiltered.filter(e => (....))
 
-      const dataFiltered = dataUnfiltered
-      chartData = dataFiltered;
-      console.log('chartData:', chartData);
+        const dataFiltered = dataUnfiltered
+        chartData = dataFiltered;
+        console.log('chartData:', chartData);
 
-      chart();
+        chart(init);
 
 
-})
+      })
+  }
 }
-main()
+main(true)
